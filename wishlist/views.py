d7c7fb8ca77413699f9d8a9e6a801239f5863ab1
Muhammad Al Rivalda <muhammad.al15@ui.ac.id>
2022-09-15 12:47:53 +0700

    cek

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
@@ -13,3 +15,20 @@ context = {
     'nama': 'Muhammad Al Rivalda'
 }
 
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+    
